1fc2608c89f2d847fc1e54e7ae3a6e2e55290055
Pravesh1010 <praveshmsp@gmail.com>
2022-03-28 19:41:48 +0530

    pizzas

--- index.js
+++ index.js
@@ -81,8 +81,115 @@ const movies = [
   },
 ];
 
+const pizzas = [
+  {
+    name: "Pepper Barbecue Chicken",
+    varients: ["small", "medium", "large"],
+    base:["Whole Wheat", "Refined Wheat", "Italian Bread", "Thin Crust", "Cheese Crust"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+  
+    category: "nonveg",
+    image:
+      "https://www.dominos.co.in/theme2/front/images/menu-images/my-vegpizza.jpg",
+    description: "Pepper Barbecue Chicken I Cheese",
+  },
+  {
+    name: "Golden Corn Pizza",
+    varients: ["small", "medium", "large"],
+    base:["Whole Wheat", "Refined Wheat", "Italian Bread", "Thin Crust", "Cheese Crust"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+
+    category: "nonveg",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6foYkrwpAO87yvGauFQzCE9O8xSi98pRWgZ0FN8SMdd3wYc0rSHad5RxZ5SI-Lh_28u0&usqp=CAU",
+    description: "Pepper Barbecue Chicken I Cheese",
+  },
+  {
+    name: "Double Cheese Margherita Pizza",
+    varients: ["small", "medium", "large"],
+    base:["Whole Wheat", "Refined Wheat", "Italian Bread", "Thin Crust", "Cheese Crust"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+
+    category: "nonveg",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_j1dPSto1BeL90inFnnD3QbaxV9qDgj_tXjj-aClRrJUaeH_LyvJ9VUI5LXstc0o4aR8&usqp=CAU",
+    description: "Pepper Barbecue Chicken I Cheese",
+  },
+  {
+    name: "Jalapeno & Red Paprika Pizza",
+    varients: ["small", "medium", "large"],
+    base:["Whole Wheat", "Refined Wheat", "Italian Bread", "Thin Crust", "Cheese Crust"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+
+    category: "nonveg",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2jHPhPJ9eAyu72WeTjf_88Z2NH5n4WAKTB1GbqriUgZHp4ncIhA_hksGUJ5Zq0kcF-Y0&usqp=CAU",
+    description: "Pepper Barbecue Chicken I Cheese",
+  },
+  {
+    name: "Non Veg Supreme",
+    varients: ["small", "medium", "large"],
+    base:["Whole Wheat", "Refined Wheat", "Italian Bread", "Thin Crust", "Cheese Crust"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+
+    category: "nonveg",
+    image:
+      "https://www.kindpng.com/picc/m/106-1066049_mexican-pizza-png-pizza-top-view-png-transparent.png",
+    description: "Pepper Barbecue Chicken I Cheese",
+  },
+  {
+    name: "Margerita",
+    varients: ["small", "medium", "large"],
+    base:["Whole Wheat", "Refined Wheat", "Italian Bread", "Thin Crust", "Cheese Crust"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+
+    category: "nonveg",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFvQHIlUrHkXO5RPTdMBnQpAWrzO3dqrjTdhNxrLFGd21eFC327AoRb1QHj9NZ09YbZX0&usqp=CAU",
+    description: "Pepper Barbecue Chicken I Cheese",
+  },
+];
+
+export default pizzas
 // middleware -> intercept -> converting body to json
 app.use(express.json());
+
 
 // Creating mongo connection //
 // const MONGO_URL = "mongodb://localhost";
@@ -162,6 +269,26 @@ app.put("/movies/:id", async function (request, response) {
     response.send(result);
   });
   
+
+  // Pizza App Backend
+app.get("/pizzas", async function(request, response){
+  const pizzas = await client
+    .db("firstmongo")
+    .collection("pizzas")
+    .find({})
+    .toArray();
+  response.send(pizzas);
+})
+
+app.post("/pizzas", async function (request, response) {
+  const data = request.body;
+  console.log(data);
+  const result = await client
+    .db("firstmongo")
+    .collection("pizzas")
+    .insertMany(data);
+  response.send(result);
+});
 
 
 app.listen(PORT, () => console.log(`Server Started in ${PORT}`));
